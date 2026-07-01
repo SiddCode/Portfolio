dd978f5f400e33963dbd266170ca460b405a658d
Sidd <siddk1045@gmail.com>
2026-07-01 15:11:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
-app.get("/", (req, res) => {
-  res.send("Portfolio Backend is Running ");
-});
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -11,6 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend is Running ");
+});
+
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
